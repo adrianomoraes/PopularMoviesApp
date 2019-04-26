8b4e1762aab7ba1627d346db35825f345fac866c
Adriano Moraes <adriano.moraes@gmail.com>
2019-04-26 12:31:20 +0100

    Código Inicial com RecyclerView e Adapter para as imagens numa Grid. Ainda não funcional.

---
+++ app/src/main/java/com/example/android/popularmoviesapp/ImageGridAdapter.java
@@ -0,0 +1,63 @@
+package com.example.android.popularmoviesapp;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class ImageGridAdapter extends RecyclerView.Adapter<ImageGridAdapter.GridItemViewHolder> {
+
+    private List<String> mImageList;
+    private Context mContext;
+
+    public class GridItemViewHolder extends RecyclerView.ViewHolder {
+        //PosterImageView posterImageView;
+        ImageView posterImageView;
+
+        public GridItemViewHolder(View view) {
+            super(view);
+            posterImageView = view.findViewById(R.id.im_poster);
+        }
+    }
+
+    public ImageGridAdapter(Context context, List imageList) {
+        mContext = context;
+        mImageList = imageList;
+    }
+
+    @Override
+    public GridItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.poster_view, parent, false);
+
+        return new GridItemViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(GridItemViewHolder holder, int position) {
+        final String path = mImageList.get(position);
+
+        Picasso.get()
+                .load(path)
+                .resize(185, 277)
+                .centerCrop()
+                .into(holder.posterImageView);
+
+        holder.posterImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //handle click event on image
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
--- app/src/main/java/com/example/android/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/example/android/popularmoviesapp/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.android.popularmoviesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ImageGridAdapter mIimageGridAdapter;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.rv_item_grid);
+        recyclerView.setHasFixedSize(true);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager (this, 2, StaggeredGridLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        List<String> imageList = new ArrayList<>();
+        imageList.add("http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg");
+        imageList.add("http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg");
+        imageList.add("http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg");
+        imageList.add("http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg");
+        imageList.add("http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg");
+        imageList.add("http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg");
+
+        mIimageGridAdapter = new ImageGridAdapter(this, imageList);
+        recyclerView.setAdapter(mIimageGridAdapter);
+
+
     }
 }
